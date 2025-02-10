7092dbaa5a0ee46581b3131d19c51ef8e7aa2a27
petar19 <41117425+petar19@users.noreply.github.com>
2025-02-10 19:09:42 +0100

    removed prints

--- main.py
+++ main.py
@@ -38,26 +38,23 @@ async def websocket_endpoint(websocket: WebSocket, room_code: str):
                 option_uuid = str(uuid.uuid4())
                 room["options"][option_uuid] = option
             elif action == "remove_option" and not room["locked"]:
-                print('remove', data)
                 option_uuid = data.get("option_uuid")
                 del room["options"][option_uuid]
-            elif action == "lock":
-                if not room["locked"]:
-                    isUserLocked = data.get('isLocked')
-                    room["users"][user_id]['locked'] = isUserLocked
-                    isRoomLocked = True
-                    for user in room["users"].values():
-                        if not user['locked']:
-                            isRoomLocked = False
-                            break
-                    room["locked"] = isRoomLocked and len(room["options"]) > 0
+            elif action == "lock" and not room["locked"]:
+                isUserLocked = data.get('isLocked')
+                room["users"][user_id]['locked'] = isUserLocked
+                isRoomLocked = True
+                for user in room["users"].values():
+                    if not user['locked']:
+                        isRoomLocked = False
+                        break
+                room["locked"] = isRoomLocked and len(room["options"]) > 0
 
-                    if room["locked"]:
-                        chosen = random.choice(list(room["options"].values())) if room["options"] else None
-                        room["choice"] = chosen
+                if room["locked"]:
+                    chosen = random.choice(list(room["options"].values())) if room["options"] else None
+                    room["choice"] = chosen
                 
             for user in room["users"].values():
-                print('updating state to users', room["options"])
                 await user['socket'].send_json({"action": "update", "options": room["options"], "locked": room["locked"]})
                 if room["locked"]: await user['socket'].send_json({"action": "result", "choice": room["choice"]})
 
